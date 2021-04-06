565139ca5ac7d8a406322aea0edffc915a7c08e7
github-action-benchmark <github@users.noreply.github.com>
2021-04-06 11:51:14 +0000

    add Rust Benchmark (cargo) benchmark result for af9092d0e0d6e1941b517ff182f346823a8ac884

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1617709874517,
+  "repoUrl": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "committer": {
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "id": "af9092d0e0d6e1941b517ff182f346823a8ac884",
+          "message": "feat: 🎸 one",
+          "timestamp": "2021-04-06T11:49:55Z",
+          "url": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs/pull/4/commits/af9092d0e0d6e1941b517ff182f346823a8ac884"
+        },
+        "date": 1617709873625,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fibonacci_20",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fibonacci_30",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
