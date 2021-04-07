54bf37ee22690240314b764ce54b556208349079
github-action-benchmark <github@users.noreply.github.com>
2021-04-07 03:08:01 +0000

    add Rust Benchmark (cargo) benchmark result for 2f5577f06c0949ca4cb5f707bdb38974f64926d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617709874517,
+  "lastUpdate": 1617764881602,
   "repoUrl": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs",
   "entries": {
     "Rust Benchmark": [
@@ -30,6 +30,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "fibonacci_30",
             "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "committer": {
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "id": "2f5577f06c0949ca4cb5f707bdb38974f64926d1",
+          "message": "feat: 🎸 fib iter",
+          "timestamp": "2021-04-06T12:46:53Z",
+          "url": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs/pull/5/commits/2f5577f06c0949ca4cb5f707bdb38974f64926d1"
+        },
+        "date": 1617764881001,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fibonacci_20",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fibonacci_30",
+            "value": 8,
             "range": "± 0",
             "unit": "ns/iter"
           }
