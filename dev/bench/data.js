87ccdc4057fbe938cb887124c560320f7d3750db
github-action-benchmark <github@users.noreply.github.com>
2022-09-21 11:45:53 +0000

    add Rust Benchmark (cargo) benchmark result for 9e66c9937eb742ca9d29e5a1a8528f793527e2f3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663758586793,
+  "lastUpdate": 1663760753329,
   "repoUrl": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs",
   "entries": {
     "Rust Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "BenchFib20",
             "value": 5,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "IWANABETHATGUY@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "committer": {
+            "email": "IWANABETHATGUY@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "distinct": true,
+          "id": "9e66c9937eb742ca9d29e5a1a8528f793527e2f3",
+          "message": "chore: 🤖 udpate bench",
+          "timestamp": "2022-09-21T19:43:52+08:00",
+          "tree_id": "c3415220b775697b28b01cc02b0dd8fa9ccbde99",
+          "url": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs/commit/9e66c9937eb742ca9d29e5a1a8528f793527e2f3"
+        },
+        "date": 1663760752902,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib20",
+            "value": 23478,
+            "range": "± 79",
             "unit": "ns/iter"
           }
         ]
