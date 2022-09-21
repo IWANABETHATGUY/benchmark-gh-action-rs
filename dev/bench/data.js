0fbf735bba742bbf924392da6e75e17a30b5174c
github-action-benchmark <github@users.noreply.github.com>
2022-09-21 11:09:46 +0000

    add Rust Benchmark (cargo) benchmark result for 70f20eccc3d5c4568b81ed2af51d7e2919130797

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1663758586793,
+  "repoUrl": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "IWANABETHATGUY@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "committer": {
+            "email": "IWANABETHATGUY@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "distinct": true,
+          "id": "70f20eccc3d5c4568b81ed2af51d7e2919130797",
+          "message": "chore: 🤖 update",
+          "timestamp": "2022-09-21T19:07:41+08:00",
+          "tree_id": "27f5430fb47ec653398f35f3b0a4cc965f3da321",
+          "url": "https://github.com/IWANABETHATGUY/benchmark-gh-action-rs/commit/70f20eccc3d5c4568b81ed2af51d7e2919130797"
+        },
+        "date": 1663758586125,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib20",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
